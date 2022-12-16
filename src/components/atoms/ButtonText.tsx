c38a3c98dc32a61e38c3f9898d86e765335099b6
Isabelle Ilyia <isabelle.ilyia@gmail.com>
2022-12-16 17:03:53 -0500

    getting plain tailwind classes to work

--- src/components/atoms/ButtonText.tsx
+++ src/components/atoms/ButtonText.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import resolveConfig from 'tailwindcss/resolveConfig';
+import tailwindConfig from '../../../tailwind.config';
+
+// const fullConfig = resolveConfig(tailwindConfig);
 
 type ButtonTextProps = {
   text: String;
 };
 
 const ButtonText: React.FC<ButtonTextProps> = ({ text }) => {
-  return <p style={{ color: 'red' }}>{text}</p>;
+  return <p className='text-2xl font-bold underline '>{text}</p>;
 };
 export default ButtonText;
--- src/components/molecules/ToolbarButton.tsx
+++ src/components/molecules/ToolbarButton.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import ButtonText from '../atoms/ButtonText';
 
 export type ToolbarButtonProps = {
