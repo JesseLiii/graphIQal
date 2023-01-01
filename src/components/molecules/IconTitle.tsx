87b715355dd2d5f26f022598d4e4d30ba328baa7
Isabelle Ilyia <isabelle.ilyia@gmail.com>
2023-01-01 16:39:32 +0200

    small fix

--- src/components/molecules/IconTitle.tsx
+++ src/components/molecules/IconTitle.tsx
@@ -6,7 +6,7 @@ type IconTitleProps = {
 };
 const IconTitle: React.FC<IconTitleProps> = ({ icon, title }) => {
   return (
-    <div className='flex flex-row content-center items-center self-center overflow-hidden'>
+    <div className='flex flex-row content-center items-center self-center absolute overflow-hidden'>
       {icon}
       <h3 className='ml-xxs'>{title}</h3>
     </div>
